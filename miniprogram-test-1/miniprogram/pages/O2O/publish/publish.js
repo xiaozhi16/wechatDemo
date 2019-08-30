e104e6003a34fd44e95aa0ef0a15bb5efa11f7f5
mrx16 <1761364057@qq.com>
2019-08-30 10:47:59 +0800

    第二个页面完成

--- miniprogram-test-1/miniprogram/pages/O2O/publish/publish.js
+++ miniprogram-test-1/miniprogram/pages/O2O/publish/publish.js
@@ -5,14 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    addImageSrc:"/images/O2O/select_image.png",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showShareMenu({
+      withShareTicket: true
+    })
   },
 
   /**
@@ -26,7 +28,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
   },
 
   /**
@@ -62,5 +63,21 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  onSelectImage:function(event){
+    var that=this;
+    wx.chooseImage({
+      count:1,
+      success:function(result){
+        that.setData({
+          addImageSrc:result.tempFilePaths
+        })
+      },
+      fail:function(error){
+        that.setData({
+          addImageSrc: "/images/O2O/select_image.png"
+        })
+      }
+    })
   }
 })
